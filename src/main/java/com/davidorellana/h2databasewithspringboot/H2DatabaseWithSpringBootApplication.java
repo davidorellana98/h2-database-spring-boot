341e058d86c59e0846152980e0d37e2430ca481a
David Orellana <davidorellana_98@hotmail.com>
2022-11-27 12:29:42 -0500

    Initial commit

---
+++ src/main/java/com/davidorellana/h2databasewithspringboot/H2DatabaseWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.davidorellana.h2databasewithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DatabaseWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DatabaseWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidorellana/h2databasewithspringboot/H2DatabaseWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidorellana.h2databasewithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DatabaseWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
