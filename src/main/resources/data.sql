c2de568964e0183587f260ca7c63710f48c0c992
David Orellana <davidorellana_98@hotmail.com>
2022-11-27 18:54:33 -0500

    Creating initial data in the data.sql file for test.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(name, last_name, age, email, date_birth) VALUES('Luis', 'Orellana', 24, 'luis@mail.com', '1998-03-27');
+INSERT INTO users(name, last_name, age, email, date_birth) VALUES('Harrison', 'Martinez', 28, 'harrison@mail.com', '1994-04-23');
+INSERT INTO users(name, last_name, age, email, date_birth) VALUES('Santiago', 'Carrillo', 28, 'santiago@mail.com', '1994-05-17');
+INSERT INTO users(name, last_name, age, email, date_birth) VALUES('Felipe', 'Calvache', 22, 'felipe@mail.com', '2000-03-27');
+INSERT INTO users(name, last_name, age, email, date_birth) VALUES('Rolando', 'Perez', 20, 'rolando@mail.com', '2002-03-27');
